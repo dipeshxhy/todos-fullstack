e3885d0cd4b32ef85257b391f7d49e2e8dfeeff8
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-07-01 04:02:06 +0545

    fix(validate): correct field names in user registration schema

--- src/validators/validate.js
+++ src/validators/validate.js
@@ -2,7 +2,8 @@ import z from 'zod';
 
 export const registerUserInputSchema = z.object({
   username: z.string().min(3, 'Username must be at least 3 characters long').trim().toLowerCase(),
-  fullName: z.string().min(3, 'Full name must be at least 3 characters long'),
+  firstName: z.string().min(3, 'First name must be at least 3 characters long'),
+  lastName: z.string().min(3, 'Last name must be at least 3 characters long'),
   email: z.email('Invalid email address').trim().toLowerCase(),
   password: z.string().min(6, 'Password must be at least 6 characters long'),
 });
